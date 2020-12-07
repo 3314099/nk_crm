d17c78595000861fa6ad27074d3b0660eb737c3e
Andrey Petrov <3314099@mail.ru>
2020-12-07 22:57:56 +0300

    обновил ноду и nuxt

--- api/keys/keys.prod.js
+++ api/keys/keys.prod.js
@@ -1,4 +1,8 @@
+// module.exports = {
+//   MONGO_URI: process.env.MONGO_URI,
+//   JWT: process.env.JWT
+// }
 module.exports = {
-  MONGO_URI: process.env.MONGO_URI,
-  JWT: process.env.JWT
+  MONGO_URI: 'mongodb+srv://Andrey3314099:Andrey2500812@nkcrm.sfipp.mongodb.net/nkcrm?retryWrites=true&w=majority',
+  JWT: 'dev-jwt-key'
 }
